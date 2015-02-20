1b9be84580c926c742c3b6d50463f8d20d5cb8be
Jason Laster <jason.laster.11@gmail.com>
2015-02-20 17:39:49 -0500

    Lots of stylistic tweaks

--- jquery.elastic.source.js
+++ jquery.elastic.source.js
@@ -11,10 +11,10 @@
 *	@licence						MIT License - http://www.opensource.org/licenses/mit-license.php
 */
 
-(function($){ 
-    $.fn.extend({  
+(function($) {
+  $.fn.extend({
 		elastic: function() {
-		
+
 			//	We will create a div clone of the textarea
 			//	by copying these attributes from the textarea to the div.
 			var mimics = [
@@ -40,16 +40,16 @@
 				'borderLeftStyle',
 				'borderLeftColor'
 				];
-			
+
 			return this.each( function() {
 
 				// Elastic only works on textareas
 				if ( this.type !== 'textarea' ) {
 					return false;
 				}
-					
-			var $textarea	= $(this),
-				$twin		= $('<div />').css({
+
+  			var $textarea	= $(this),
+				$twin	= $('<div />').css({
 					'position'		: 'absolute',
 					'display'		: 'none',
 					'word-wrap'		: 'break-word',
@@ -59,59 +59,59 @@
 				minheight	= parseInt($textarea.css('height'),10) || lineHeight*3,
 				maxheight	= parseInt($textarea.css('max-height'),10) || Number.MAX_VALUE,
 				goalheight	= 0;
-				
+
 				// Opera returns max-height of -1 if not set
 				if (maxheight < 0) { maxheight = Number.MAX_VALUE; }
-					
+
 				// Append the twin to the DOM
 				// We are going to meassure the height of this, not the textarea.
 				$twin.appendTo($textarea.parent());
-				
+
 				// Copy the essential styles (mimics) from the textarea to the twin
 				var i = mimics.length;
 				while(i--){
 					$twin.css(mimics[i].toString(),$textarea.css(mimics[i].toString()));
 				}
-				
+
 				// Updates the width of the twin. (solution for textareas with widths in percent)
 				function setTwinWidth(){
 					var curatedWidth = Math.floor(parseInt($textarea.width(),10));
 					if($twin.width() !== curatedWidth){
 						$twin.css({'width': curatedWidth + 'px'});
-						
+
 						// Update height of textarea
 						update(true);
 					}
 				}
-				
+
 				// Sets a given height and overflow state on the textarea
 				function setHeightAndOverflow(height, overflow){
-                    if($textarea.data('cancel-resize') === "true") {
-                        return;
-                    }
+          if($textarea.data('cancel-resize') === "true") {
+              return;
+          }
 					var curratedHeight = Math.floor(parseInt(height,10));
 					if($textarea.height() !== curratedHeight){
 						$textarea.css({'height': curratedHeight + 'px','overflow':overflow});
 					}
 				}
-				
-				// This function will update the height of the textarea if necessary 
+
+				// This function will update the height of the textarea if necessary
 				function update(forced) {
-					
+
 					// Get curated content from the textarea.
 					var textareaContent = $textarea.val().replace(/&/g,'&amp;').replace(/ {2}/g, '&nbsp;').replace(/<|>/g, '&gt;').replace(/\n/g, '<br />');
-					
+
 					// Compare curated content with curated twin.
 					var twinContent = $twin.html().replace(/<br>/ig,'<br />');
-					
+
 					if(forced || textareaContent+'&nbsp;' !== twinContent){
-					
+
 						// Add an extra white space so new rows are added when you are at the end of a row.
 						$twin.html(textareaContent+'&nbsp;');
-						
+
 						// Change textarea height if twin plus the height of one line differs more than 3 pixel from textarea height
 						if(Math.abs($twin.height() + lineHeight - $textarea.height()) > 3){
-							
+
 							var goalheight = $twin.height()+lineHeight;
 							if(goalheight >= maxheight) {
 								setHeightAndOverflow(maxheight,'auto');
@@ -120,54 +120,52 @@
 							} else {
 								setHeightAndOverflow(goalheight,'hidden');
 							}
-							
+
 						}
-						
+
 					}
-					
 				}
-				
-                //Detects manual resize
-                function removeAutoResizeOnManualResize() {
-                    $textarea.unbind(".manualresize");
-                    $(document).unbind(".manualresize");
-                    
-                    var mousedownListener = function() {
-                        var taTemp = $(this);
-                        taTemp.data('x', taTemp.outerWidth());
-                        taTemp.data('y', taTemp.outerHeight());
-                    };
-                    $textarea.bind("mousedown.manualresize", mousedownListener);
-                    
-                    var mouseupListener = function() {
-                        if ($textarea.outerWidth() != $textarea.data('x') || $textarea.outerHeight() != $textarea.data('y'))
-                        {
-                            $textarea.css("overflow-y", "auto");
-                            $textarea.data('cancel-resize', "true");
-                        }
-                    };
-                    $(document).bind("mouseup.manualresize", mouseupListener);
-                };
+
+        //Detects manual resize
+        function removeAutoResizeOnManualResize() {
+          $textarea.unbind(".manualresize");
+          $(document).unbind(".manualresize");
+          $textarea.bind("mousedown.manualresize", mousedownListener);
+          $(document).bind("mouseup.manualresize", mouseupListener);
+
+          var mousedownListener = function() {
+            var taTemp = $(this);
+            taTemp.data('x', taTemp.outerWidth());
+            taTemp.data('y', taTemp.outerHeight());
+          };
+
+          var mouseupListener = function() {
+            if ($textarea.outerWidth() != $textarea.data('x') || $textarea.outerHeight() != $textarea.data('y')) {
+              $textarea.css("overflow-y", "auto");
+              $textarea.data('cancel-resize', "true");
+            }
+          };
+        };
 
 				// Hide scrollbars
 				$textarea.css({'overflow':'hidden'});
-				
+
 				// Update textarea size on keyup, change, cut and paste
 				$textarea.bind('keyup change cut paste', function(){
-					update(); 
+					update();
 				});
-				
+
 				// Update width of twin if browser or textarea is resized (solution for textareas with widths in percent)
-                $(window).bind('resize', setTwinWidth);
+        $(window).bind('resize', setTwinWidth);
 				$textarea.bind('resize', setTwinWidth);
-                $textarea.bind('focus', setTwinWidth);
+        $textarea.bind('focus', setTwinWidth);
 				$textarea.bind('update', update);
-				
+
 				// Compact textarea on blur
 				$textarea.bind('blur',function(){
-                    if($textarea.data('cancel-resize') === "true") {
-                        return;
-                    }
+          if($textarea.data('cancel-resize') === "true") {
+            return;
+          }
 					if($twin.height() < maxheight){
 						if($twin.height() > minheight) {
 							$textarea.height($twin.height());
@@ -177,23 +175,18 @@
 					}
 				});
 
-                //Cancel auto resize on detection of manual resize 
-                $textarea.bind('focus', function ()
-                {
-                    setTimeout(function ()
-                    {
-                        removeAutoResizeOnManualResize();
-                    }, 1000);
+        //Cancel auto resize on detection of manual resize
+        $textarea.bind('focus', function () {
+          setTimeout(removeAutoResizeOnManualResize, 1000);
 				});
-				
+
 				// And this line is to catch the browser paste event
-				$textarea.bind('input paste',function(e){ setTimeout( update, 250); });				
-				
+				$textarea.bind('input paste',function(e){ setTimeout( update, 250); });
+
 				// Run update once when elastic is initialized
 				update();
-				
+
 			});
-			
-        } 
-    }); 
+    }
+  });
 })(jQuery);
